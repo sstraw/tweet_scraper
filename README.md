e5cfd500ce67065b94a1d8cebeb52671e1da0338
sstraw <sean.r.straw@gmail.com>
2018-12-30 11:11:36 -0800

    Pushing twitter scraper

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # tweet_scraper
 Python script/library for scraping tweets from a twitter user.
+
+## Usage
+```
+>>> import tweet_scraper
+>>> for t in tweet_scraper.retrieve('scumbots', n_tweets=2):
+...     print (json.dumps(t, indent=2))
+...
+{
+  "tweet_id": "1079434449635500032",
+  "link": "/ScumBots/status/1079434449635500032",
+  "text": "PowerShell_Empire_Agent found at https://pastebin.com/FtfFsAet\u00a0SHA256: dec16433087bf7927059980265bae41fc0bee12266dc4d6d25c21d1b805de458 C2: http://147[.]135[.]237[.]28:80",
+  "timestamp": "2018-12-30T09:50:02"
+}
+{
+  "tweet_id": "1079433699232530433",
+  "link": "/ScumBots/status/1079433699232530433",
+  "text": "njRat found at https://pastebin.com/0u2Bb5kE\u00a0 SHA256: d9db436b3e5910c0f878d0517ef0a09e40cd2d2bfa69701367a3f73d17fbd007 C2: tcp://dollar[.]ddns[.]net:2604",
+  "timestamp": "2018-12-30T09:47:03"
+}
+```
+
+### Command line
+```
+$ python3 ./tweet_scraper.py -h
+usage: tweet_scraper.py [-h] [--max-id MAX_ID] [--min-id MIN_ID] [-n N]
+                        [--max-requests MAX_REQUESTS]
+                        username
+
+Simple script for scraping twitter user pages. Inspired by
+https://github.com/kennethreitz/twitter-scraper
+
+positional arguments:
+  username              Username of page to scrape
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --max-id MAX_ID       Max tweet ID to pull (Non-inclusive)
+  --min-id MIN_ID       Min tweet ID to pull (Non-inclusive)
+  -n N                  Max number of tweets to retrieve
+  --max-requests MAX_REQUESTS
+                        Max number of requests to make. One request gets at
+                        most 20 tweets
+lime:tweet_scraper sean$ python3 ./tweet_scraper.py -n 2 scumbots
+{"tweet_id": "1079434449635500032", "link": "/ScumBots/status/1079434449635500032", "text": "PowerShell_Empire_Agent found at https://pastebin.com/FtfFsAet\u00a0 SHA256: dec16433087bf7927059980265bae41fc0bee12266dc4d6d25c21d1b805de458 C2: http://147[.]135[.]237[.]28:80", "timestamp": "2018-12-30T09:50:02"}
+{"tweet_id": "1079433699232530433", "link": "/ScumBots/status/1079433699232530433", "text": "njRat found at https://pastebin.com/0u2Bb5kE\u00a0 SHA256: d9db436b3e5910c0f878d0517ef0a09e40cd2d2bfa69701367a3f73d17fbd007 C2: tcp://dollar[.]ddns[.]net:2604", "timestamp": "2018-12-30T09:47:03"}
+```
+
+## Credit
+Derived largely from work by https://github.com/kennethreitz/twitter-scraper. I reimplemented the script to better suit some further functionality I needed and to reduce the library footprint.
---
+++ tweet_scraper.py
@@ -0,0 +1,145 @@
+"""
+tweet_scraper.py
+author: github.com/sstraw
+source: github.com/sstraw/tweet_scraper
+desc  : Simple module for scraping tweets from a twitter user.
+credit:
+Twitter API access derived from work by:
+https://github.com/kennethreitz/twitter-scraper
+"""
+import bs4
+import requests
+from datetime import datetime
+
+def retrieve(
+    user,
+    min_id   = None, 
+    max_id   = None,
+    n_tweets = 20,
+    max_reqs = None
+):
+    """
+    Generator that retrieves a given user's tweets. Can use min_id
+    and max_id to specify which tweets to fetch, which are non-
+    inclusive. n_tweets specifies most tweets to fetch
+    (Default 20). Set n_tweets to None for no limit.
+    max_reqs is used to specify a maximum number of requests
+    to the api
+    """
+
+    url = ''.join((
+        'https://twitter.com/i/profiles/show/',
+        user,
+        '/timeline/tweets',
+    ))
+
+    params = {
+        'include_available_features' : 1,
+        'include_entities'           : 1,
+        'include_new_items_bar'      : "true"
+    }
+
+    if max_id:
+        params['max_position'] = max_id
+    if min_id:
+        params['min_position'] = min_id
+
+    retrieved_tweets = 0
+    n_requests       = 0
+
+    with requests.Session() as r_session:
+        while True:
+            r           = r_session.get(url, params=params)
+            n_requests += 1
+            r_json      = r.json()
+
+            for t in parse(r_json['items_html']):
+                retrieved_tweets += 1
+                yield t
+
+                if (n_tweets != None and
+                    retrieved_tweets >= n_tweets
+                ):
+                    return
+
+            if r_json['has_more_items'] == False:
+                return
+
+            if (max_reqs != None and
+                max_reqs <= n_requests
+            ):
+                return
+
+            params['max_position'] = r_json['min_position']
+
+
+def parse(html):
+    """
+    Generator function. Given the provided HTML, loop through
+    and return dictionary objects of tweets containing the
+    following:
+        -tweet_id
+        -link
+        -text
+        -timestamp
+    """
+    soup = bs4.BeautifulSoup(
+        html,
+        'html.parser'
+    )
+
+    for stream_item in soup.find_all(class_='stream-item'):
+        tweet = dict()
+
+        tweet_item = stream_item.find(class_ = 'tweet')
+        tweet_text = stream_item.find(class_ = 'tweet-text')
+        time_item  = stream_item.find(class_ = '_timestamp')
+
+        tweet['tweet_id']  = stream_item['data-item-id']
+        tweet['link']      = tweet_item ['data-permalink-path']
+        tweet['text']      = tweet_text.text
+        
+        _timestamp = datetime.fromtimestamp(int(time_item['data-time'])) 
+        tweet['timestamp'] = _timestamp.isoformat()
+
+        yield tweet 
+
+
+def main():
+    """
+    Main function if called from command line
+    """
+    import argparse
+    import json
+
+    parser = argparse.ArgumentParser(
+        description=("Simple script for scraping twitter user pages. "
+                     "Inspired by "
+                     "https://github.com/kennethreitz/twitter-scraper")
+    )
+    parser.add_argument('username', help='Username of page to scrape')
+    parser.add_argument('--max-id',       default=None,
+                        help='Max tweet ID to pull (Non-inclusive)')
+    parser.add_argument('--min-id',       default=None,
+                        help='Min tweet ID to pull (Non-inclusive)')
+    parser.add_argument('-n',             default=20, type=int,
+                        help='Max number of tweets to retrieve')
+    parser.add_argument('--max-requests', default=None,
+                        help=('Max number of requests to make. '
+                              'One request gets at most 20 tweets'))
+
+    args = parser.parse_args()
+
+    for tweet in retrieve(
+        args.username,
+        max_id = args.max_id,
+        min_id = args.min_id,
+        n_tweets = args.n,
+        max_reqs = args.max_requests
+    ):
+        print (json.dumps(tweet))
+
+                          
+
+if __name__ == '__main__':
+    main()
